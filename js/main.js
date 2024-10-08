f98c600d6cafa8119247cd8b4e826f8fa3935960
owlstechbd <owlstech.net@gmail.com>
2024-10-08 22:31:34 +0600

    dsfghsdf

--- js/main.js
+++ js/main.js
@@ -431,8 +431,3 @@ window.addEventListener('click', (event) => {
     popup.style.display = 'none';
   }
 });
-
-window.onload = function() {
-  // Immediately hide the popup when the user first visits the site
-  document.getElementById('popupForm').style.display = 'none';
-};
